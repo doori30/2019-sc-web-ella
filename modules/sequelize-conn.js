a16b8989042b340883a5649e3fd3cd162f9af114
Minkyung Kim <mminkyung.k@gmail.com>
2021-03-28 18:38:09 +0900

    sequelize-conn 수정

--- modules/sequelize-conn.js
+++ modules/sequelize-conn.js
@@ -2,22 +2,22 @@
 const Sequelize = require("sequelize");//<script src="../js/fx-slide.js"></script>
 //대문자▲ 소문자▼
 const sequelize = new Sequelize({
-	host: "db.doori30.gabia.io",
-	// host: "localhost",
-	port: 3306,
-	// database:"ella",
-	// username:"ella",
-	// password:"000000",
-	username : "doori30",
-	password : "tmdnlxl1004!!",
-	database : "dbdoori30",
-	dialect:"mysql",
-	pool:{
-		max:10,
-		min:0,
-		acquire:30000,
-		idle:10000
-	}//option(접속정보)
+  host: "ls-933041b199472ed4b7e9d975a30de433220bd814.clv2i0roeqfa.ap-northeast-2.rds.amazonaws.com",
+// host: "localhost",
+  port: 3306,
+// database:"ella",
+// username:"ella",
+// password:"000000",
+  username: "dbmasteruser",
+  password: "v%~KGvaPe~m+90;jq5a(9Q{S1vFCHa1[",
+  database: "dbmaster",
+  dialect: "mysql",
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }//option(접속정보)
 });
 
 // sequelize
@@ -29,14 +29,13 @@ const sequelize = new Sequelize({
 // 		console.error('Unable to connect to the database:', err);
 // 	});
 
-(async()=>{
-	try{
-	await sequelize.authenticate();
-	console.log('Connection has been established successfully.')
-	}
-	catch(err){
-		console.error('Unable to connect to the database:', err);
-	}
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.')
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+  }
 })();
 
-module.exports ={Sequelize,sequelize}//추출함
+module.exports = {Sequelize, sequelize}//추출함
